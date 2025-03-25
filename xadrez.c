7e2e998789c1b1d80cf599630121abdc3cb6f409
Starvixs <202503614776@alunos.estacio.br>
2025-03-25 01:53:29 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+//Função recursiva para a torre
+void moverTorre(int casas){
+    if (casas > 0 && casas < 9){
+        printf("Direita\n");
+        moverTorre(casas - 1);
+    }
+}
+//Função recursiva para o bispo
+void moverBispo(int casas){
+    if (casas > 0 && casas < 9){
+        printf("Cima\n");
+        printf("Direita\n");
+        moverBispo(casas - 1);    
+    }
+}
+//Função recursiva para a rainha
+void moverRainha(int casas){
+    if (casas > 0 && casas < 9){
+        printf("Esquerda\n");
+        moverRainha(casas - 1);
+    }
+}
+    
+
 
 int main() {
     int escolha;
 
-    do {
+    do { //Opções para a entrada de qual peça deseja utilizar
         printf("Escolha qual peça quer mover\n"
+               "0. Sair do jogo\n"
                "1. Torre\n"
                "2. Bispo\n"
                "3. Rainha\n"
@@ -12,23 +37,23 @@ int main() {
         scanf("%d", &escolha); //Leitura da peça que deseja escolher
 
         switch (escolha) { //Menu de escolha pra qual peça deseja escolher
+            case 0:printf("Saindo do jogo...\n"); break;
             case 1:
-                for (int torre = 0; torre < 5; torre++) { // Peça, Quantidade de movimentos da peça, e a Movimentação da peça
-                    printf("Direita\n");
-                }
+                printf("***Movendo Torre***\n");
+                moverTorre(5); //Recursividade da torre
+                printf("\n");
                 break;
 
             case 2:
-                for (int bispo = 0; bispo < 5; bispo++) { // Peça, Quantidade de movimentos da peça, e a Movimentação da peça
-                    printf("Cima\n");
-                    printf("Direita\n");
-                }
+                printf("***Movendo Bispo***\n"); //Recursividade do bispo
+                moverBispo(5);
+                printf("\n");
                 break;
 
             case 3:
-                for (int rainha = 0; rainha < 8; rainha++) { // Peça, Quantidade de movimentos da peça, e a Movimentação da peça
-                    printf("Esquerda\n");
-                }
+                printf("***Movendo Rainha***\n"); //Recursividade da rainha
+                moverRainha(5);
+                printf("\n");
                 break;
 
             case 4:
